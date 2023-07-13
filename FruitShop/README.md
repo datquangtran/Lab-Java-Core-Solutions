06c6cb93f2ff836540612aefdfd16a0b4aedcb61
datquangtran <45448911+datquangtran@users.noreply.github.com>
2023-07-14 05:47:17 +0700

    Delete FruitShop directory

--- FruitShop/README.md
+++
@@ -1,6 +0,0 @@
-# J1.L.P0023_FruitShop
-
-
-## Issue
-
-- Khi `quantity = 0` sẽ hiện message là **_Only choice [1 - 0]_** => Không hợp lý, cần sửa lại.
--- FruitShop/src/util/IConstant.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public interface IConstant {
-    String CUSTOMER_NAME = "^[a-zA-Z ]+$";
-    String FRUIT_NAME = "^[a-zA-Z0-9 \\-]+$";
-    String ORIGIN = "^[a-zA-Z0-9 \\-,]+$";
-    String YES_NO = "^[yYnN]$";
-}
